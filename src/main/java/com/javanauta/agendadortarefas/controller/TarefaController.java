c24ab39163bad42c0ca5438f7fff3375ad13ec1f
Alessandro <Alessandro.klein13@yahoo.com.br>
2026-04-05 14:36:44 -0300

    Adicionado método de alterar tarefa e deletar

--- src/main/java/com/javanauta/agendadortarefas/controller/TarefaController.java
+++ src/main/java/com/javanauta/agendadortarefas/controller/TarefaController.java
@@ -3,6 +3,7 @@ package com.javanauta.agendadortarefas.controller;
 
 import com.javanauta.agendadortarefas.business.TarefaService;
 import com.javanauta.agendadortarefas.business.dto.TarefaDTO;
+import com.javanauta.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
 import com.javanauta.agendadortarefas.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -22,25 +23,44 @@ public class TarefaController {
     private final JwtUtil jwtUtil;
 
 
-
     @PostMapping
-    public ResponseEntity<TarefaDTO> salvaTarefa (@RequestBody TarefaDTO tarefaDTO,
-                                                  @RequestHeader("Authorization") String token){
+    public ResponseEntity<TarefaDTO> salvaTarefa(@RequestBody TarefaDTO tarefaDTO,
+                                                 @RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(tarefaService.gravarTarefa(token, tarefaDTO));
     }
 
     @GetMapping("/eventos")
     public ResponseEntity<List<TarefaDTO>> buscaListadeTarefasPorPeriodo(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dataInicial,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)LocalDateTime dataFinal
-            ){
-        return ResponseEntity.ok(tarefaService.buscaTarefasAgendadasPorPeriodo(dataInicial,dataFinal));
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicial,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFinal
+    ) {
+        return ResponseEntity.ok(tarefaService.buscaTarefasAgendadasPorPeriodo(dataInicial, dataFinal));
     }
 
     @GetMapping("/user")
     public ResponseEntity<List<TarefaDTO>> buscarPorEmail(
-            @RequestHeader("Authorization") String token){
+            @RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(tarefaService.buscarporUser(token));
     }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarPorId(@RequestParam("id") String id) {
+        tarefaService.excluirPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<TarefaDTO> alteraStatusNotificacao(@RequestParam("status") StatusNotificacaoEnum status,
+                                                             @RequestParam("id") String id) {
+        return ResponseEntity.ok(tarefaService.alteraStatus(status, id));
+    }
+
+    @PutMapping
+    public ResponseEntity<TarefaDTO> editarTarefa(@RequestBody TarefaDTO dto,
+                                                  @RequestParam("id") String id){
+        return ResponseEntity.ok(tarefaService.updateTarefas(dto,id));
+    }
+
+
 
 }
